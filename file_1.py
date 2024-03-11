ece7e6de65ac750f1a8a5c8277199a7b5c78177b
Yarraguntlasushma <160005603+Yarraguntlasushma@users.noreply.github.com>
2024-03-11 16:01:51 +0530

    Add files via upload

---
+++ file_1.py
@@ -0,0 +1,45 @@
+'''
+Bargained characters:
+controls and data structures->ABC language
+strings and its formats->from c
+Regular expression->pear 1
+Exception handling->Module 3
+magic methods from->fortron
+What kind of language is python?
+python is a
+>Object oriented programing Language
+>Dynamically typed Language
+>Interpreted Language
+>Complied Language
+!-->variables
+variable is a name which is used to identify the value of variable
+?which is stored in a specific memory location
+Rules to define variables
+variable have to start with only alphabets,underscores
+The char following the 1st char can be alphabets,underscores,numbers
+No special characters like 1@#$%^&*()+
+case sensitive
+No spaces are allowed
+keywords are not allowed as variables
+'''
+
+#print("hello world")
+#print("hello world")
+'''
+print("hello world")
+print("hello world")
+'''
+#print("hello world")
+
+#import this
+#print(this)
+
+#import antigravity
+'''
+a=2
+b=3
+c=23
+print(a,b,c)
+'''
+a,b=78
+print(a,b)
